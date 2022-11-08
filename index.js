7a2173468fb4c9074167412074ef8e078cf185a1
PEPEMAN <raghavwastaken1@gmail.com>
2022-11-08 13:38:16 +0530

    Vercel Api Created at /api/vercel/projects

---
+++ index.js
@@ -0,0 +1,45 @@
+const dotenv = require("dotenv");
+const axios = require("axios");
+dotenv.config();
+
+const vercelToken = process.env.VERCEL_TOKEN;
+
+const apiEndPt = "https://api.vercel.com/v9/projects";
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/api/vercel/projects", (req, res) => {
+    let config = {
+        method: "get",
+        url: apiEndPt,
+        headers: {
+            Authorization: "Bearer " + vercelToken,
+        },
+    };
+
+    let results = [];
+
+    axios(config)
+        .then((response) => {
+            for (let project of response.data.projects) {
+                results.push({
+                    name: project.name,
+                    githubRepo: `https://github.com/${project.link.org}/${project.link.repo}`,
+                    link: project.latestDeployments[0].alias[0],
+                });
+            }
+
+            if (response.data.pagination.next !== null) {
+                config.url = `${apiEndPt}?until=${response.data.pagination.next}`;
+                loop();
+            } else {
+                res.status(200).json(results);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+app.listen(port, () => console.log(`Example app listening on port ${port} !`));
